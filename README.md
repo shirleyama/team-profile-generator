a239f45c5fa5d43fe4bf8330451012bb23851b4e
Ama Ama <quists@hotmail.com>
2023-01-31 22:14:51 +0000

    code to write output file

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# team-profile-generator
-An application that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.
+# Team Profile Generator
+
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+This application will take in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.
+
+## Table of Contents
+
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Contributing](#contributing)
+4. [Tests](#tests)
+5. [Questions](#questions)
+
+## Installation
+
+Load it in a node environment and run with node index.js
+
+## Usage
+
+node index.js
+
+## License
+
+Licensed under the MIT license.
+
+## Contributing
+
+shirleyama
+
+## Tests
+
+Locally on my machine on the command line and using JEST too
+
+## Questions
+
+If you have any questions, please feel free to reach out to me via:
+
+- [shirleyama](https://github.com/shirleyama)
+- [quists@hotmail.com](mailto:quists@hotmail.com)
--- index.js
+++ index.js
@@ -9,7 +9,138 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
+// if output directory does not exist create it
+if (!fs.existsSync(OUTPUT_DIR)) {
+  fs.mkdirSync(OUTPUT_DIR);
+}
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+//engineer type create
+const engineerQuestion = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the engineer's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the engineer's id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the engineer's email?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's Github username?",
+  },
+];
 
+//intern type create
+const internQuestion = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the intern's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the intern's id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the intern's email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is the intern's school?",
+  },
+];
+
+// type manager create, and then choose member type
+const teamMembers = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "What is the manager's name?",
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "What is the manager's id?",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "What is the manager's email?",
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "What is the manager's office number?",
+    },
+  ])
+  .then((managerAnswer) => {
+    teamMembers.push(
+      new Manager(
+        managerAnswer.managerName,
+        managerAnswer.managerId,
+        managerAnswer.managerEmail,
+        managerAnswer.managerOfficeNumber
+      )
+    );
+
+    addEmployee();
+  });
+
+const finishBuildingTeam = () => {
+  fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+};
+
+//choose emplyee type
+const addEmployee = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "addEmployeeType",
+      message: "Choose an employee type?",
+      choices: ["Engineer", "Intern", "Create team now"],
+    })
+    .then(({ addEmployeeType }) => {
+      if (addEmployeeType === "Engineer") {
+        inquirer.prompt(engineerQuestion).then((engineerAnswer) => {
+          teamMembers.push(
+            new Engineer(
+              engineerAnswer.name,
+              engineerAnswer.id,
+              engineerAnswer.email,
+              engineerAnswer.github
+            )
+          );
+          addEmployee();
+        });
+      } else if (addEmployeeType === "Intern") {
+        inquirer.prompt(internQuestion).then((internAnswer) => {
+          teamMembers.push(
+            new Intern(
+              internAnswer.name,
+              internAnswer.id,
+              internAnswer.email,
+              internAnswer.school
+            )
+          );
+          addEmployee();
+        });
+      } else if (addEmployeeType === "Create team now") {
+        finishBuildingTeam();
+      }
+    });
+};
