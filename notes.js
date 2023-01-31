bf8430581127139c76565b709dcbf9cf54c3b23a
Ama Ama <quists@hotmail.com>
2023-01-31 01:46:27 +0000

    added in code for individual team members classes

--- notes.js
+++ notes.js
@@ -1,11 +1,26 @@
+//example code from JD
 // index.js
-const Student = require('./Student');
-const stan = new Student('UK10', '02/24/2023', 'Stan');
+const Student = require("./Student");
+const stan = new Student("UK10", "02/24/2023", "Stan");
 
 stan.printClassCode();
 
+//Course.js
+class Course {
+  constructor(class_code, grad_date) {
+    this.class_code = class_code;
+    this.grad_date = grad_date;
+  }
+  printClassCode() {
+    console.log(this.class_code);
+  }
+  printGradDate() {
+    console.log(this.grad_date);
+  }
+}
+
 // Student.js
-const Course = require('./Course');
+const Course = require("./Course");
 
 class Student extends Course {
   constructor(class_code, grad_date, name) {
@@ -21,7 +36,8 @@ class Student extends Course {
 module.exports = Student;
 
 // SSM.js
-const Course = require('./Course');
+const Course = require("./Course");
+const { conditionalExpression } = require("@babel/types");
 
 class SSM extends Course {
   constructor(class_code, grad_date, name) {
@@ -35,19 +51,3 @@ class SSM extends Course {
 }
 
 module.exports = SSM;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
