5fbda901b25c4663e1908ec6f859dedb9101024a
Ama Ama <quists@hotmail.com>
2023-02-03 22:33:53 +0000

    added style sheet for minimal styling

--- src/page-template.js
+++ src/page-template.js
@@ -5,7 +5,7 @@ const generateTeam = team => {
     const generateManager = manager => {
         return `
         <div class="card employee-card">
-        <div class="card-header">
+        <div class="manager card-header">
             <h2 class="card-title">${manager.getName()}</h2>
             <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
         </div>
@@ -23,8 +23,8 @@ const generateTeam = team => {
     // creates the html for engineers
     const generateEngineer = engineer => {
         return `
-        <div class="card employee-card">
-    <div class="card-header">
+        <div class="card  employee-card">
+    <div class="engineer card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
     </div>
@@ -43,7 +43,7 @@ const generateTeam = team => {
     const generateIntern = intern => {
         return `
         <div class="card employee-card">
-    <div class="card-header">
+    <div class="intern card-header">
         <h2 class="card-title">${intern.getName()}</h2>
         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
     </div>
